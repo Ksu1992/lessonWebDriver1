91a0f8a976841728d1f24ef4677a30321583a71b
Kseniia Naryshkina <narushkina.ksu@gmail.com>
2023-12-24 12:25:01 +0100

    Initial commit

---
+++ src/main/java/org/alevel/Lesson1.java
@@ -0,0 +1,23 @@
+package org.alevel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Lesson1 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jobs.dou.ua/");
+        WebElement input=driver.findElement(By.xpath("//input[@class='job']"));
+
+        WebElement searchButton =driver.findElement(By.xpath("//input[@value='Знайти']"));
+input.clear();
+input.sendKeys("Team lead");
+searchButton.click();
+
+driver.quit();
+
+
+    }
+}
