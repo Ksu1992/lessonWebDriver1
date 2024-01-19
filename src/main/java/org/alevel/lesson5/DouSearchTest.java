8e2917a3d961810e3c29cb3832d08a39129b0659
Kseniia Naryshkina <narushkina.ksu@gmail.com>
2024-01-19 14:16:01 +0100

    commit

--- src/main/java/org/alevel/lesson5/DouSearchTest.java
+++ src/main/java/org/alevel/lesson5/DouSearchTest.java
@@ -2,19 +2,28 @@ package org.alevel.lesson5;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+
+
+
+
 public class DouSearchTest {
     private WebDriver driver;
+
+
+
 
     @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.firefox.driver", "path/to/firefoxdriver");
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.get("https://dou.ua");
     }
 
@@ -27,7 +36,7 @@ public class DouSearchTest {
 
     @DataProvider(name = "searchQueries")
     public Object[][] createSearchQueries() {
-        return new Object[][] {
+        return new Object[][]{
                 {"Selenium"},
                 {"Java"}
         };
@@ -35,7 +44,25 @@ public class DouSearchTest {
 
     @Test(dataProvider = "searchQueries")
     public void testSearch(String query) {
+        // Сохраняем текущий URL до выполнения поиска
+        String originalUrl = driver.getCurrentUrl();
+
+       //Пошук
         driver.findElement(By.name("q")).sendKeys(query);
         driver.findElement(By.xpath("//input[@id='txtGlobalSearch']")).click();
+
+
+        // Получение нового URL
+        String newUrl = driver.getCurrentUrl();
+
+        // Проверка,что URL остался прежним
+        Assert.assertEquals(newUrl, originalUrl, "URL изменился после выполнения поиска.");
     }
+
 }
+
+
+
+
+
+
